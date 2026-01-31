c28411fa5e4b3b4aa7b058d5f9f09bc63efc3425
NattapongPi <nattapong.srk@gmail.com>
2026-01-31 20:38:20 +0700

    add speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 export default function RootLayout({
   children,
@@ -30,6 +31,7 @@ export default function RootLayout({
         </nav>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
